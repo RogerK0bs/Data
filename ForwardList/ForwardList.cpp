f70e73cf5086120bc865475726d8d17dc56b521d
Leon9 <Leon9@K0_BS>
2023-03-20 22:54:23 +0700

    List_separated

---
+++ ForwardList/ForwardList.cpp
@@ -0,0 +1,171 @@
+﻿#include"ForwardList.h"
+///-------------------------------------------------------------------------------------///
+
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////			CLASS DEFINITION - ÎÏÐÅÄÅËÅÍÈÅ ÊËÀÑÑÀ				///////////////
+
+template<typename T>int Element<T>::count = 0;	//Èíèöèàëèçèðóåì ñòàòè÷åñêó ïåðåìåííóþ, îáúÿâëåííóþ â êëàññå 'Element'
+
+template<typename T>Element<T>::Element(T Data, Element<T>* pNext) :Data(Data), pNext(pNext)
+{
+	count++;
+	cout << "EConstructor:\t" << this << endl;
+}
+template<typename T>Element<T>::~Element()
+{
+	count--;
+	cout << "EDestructor:\t" << this << endl;
+}
+
+//////////////////////		Iterator		////////////////////////////////
+template<typename T>Iterator<T>::Iterator(Element<T>* Temp) :Temp(Temp)
+{
+	cout << "ItConstructor:\t" << this << endl;
+}
+template<typename T>Iterator<T>::~Iterator()
+{
+	cout << "ItDestructor:\t" << this << endl;
+}
+
+template<typename T>Iterator<T>& Iterator<T>::operator++()
+{
+	Temp = Temp->pNext;
+	return *this;
+}
+
+template<typename T>bool Iterator<T>::operator==(const Iterator<T>& other)const
+{
+	return this->Temp == other.Temp;
+}
+template<typename T>bool Iterator<T>::operator!=(const Iterator<T>& other)const
+{
+	return this->Temp != other.Temp;
+}
+template<typename T>T& Iterator<T>::operator*()
+{
+	return Temp->Data;
+}
+//////////////////////////////   ForwardList	/////////////////////////////////
+template<typename T>Iterator<T> ForwardList<T>::begin()
+{
+	return Head;
+}
+template<typename T>Iterator<T> ForwardList<T>::end()
+{
+	return nullptr;
+}
+template<typename T>ForwardList<T>::ForwardList()
+{
+	Head = nullptr;	//Åñëè ñïèñîê ïóñò, òî åãî Ãîëîâà óêàçûâàåò íà 0
+	size = 0;
+	cout << "LConstructor:\t" << this << endl;
+}
+template<typename T>ForwardList<T>::ForwardList(const std::initializer_list<T>& il) :ForwardList()
+{
+	//il - initializer_list
+	//initializer_list - ýòî êîíòåéíåð.
+	//Êîíòåéíåð - ýòî îáúåêò, êîòîðûé îðãàíèçóåò õðàíåíèå äðóãèõ îáúåêòîâ â ïàìÿòè.
+	//Êàê è ó ëþáîãî äðóãîãî êîíòåéíåðà, ó initilizer_list åñòü ìåòîäû begin() è end()
+	//begin() - âîçâðàùàåò èòåðàòîð íà íà÷àëî êîíòåéíåðà.
+	//end()   - âîçâðàùàåò èòåðàòîð íà êîíåö êîíòåéíåðà.
+	cout << typeid(il.begin()).name() << endl;
+	for (T const* it = il.begin(); it != il.end(); it++)
+	{
+		push_back(*it);
+	}
+}
+template<typename T>ForwardList<T>::~ForwardList()
+{
+	while (Head)pop_front();
+	cout << "LDestructor:\t" << this << endl;
+}
+
+//					Adding elements:
+template<typename T>void ForwardList<T>::push_front(T Data)
+{
+	/*
+	//1) Ñîçäàåì íîâûé ýëåìåíò:
+	Element* New = new Element(Data);
+	//2) Íîâûé ýëåìåíò äîëæåí óêàçûâàòü íà íà÷àëî ñïèñêà:
+	New->pNext = Head;
+	//3) Ãîëîâó ñïèñêà "ïåðåâîäèì" íà íîâûé ýëåìåíò:
+	Head = New;
+	*/
+	Head = new Element<T>(Data, Head);
+	size++;
+}
+template<typename T>void ForwardList<T>::push_back(T Data)
+{
+	if (Head == nullptr)return push_front(Data);
+	//1) Ñîçäàåì íîâûé ýëåìåíò:
+	//Element* New = new Element(Data);
+	//2) Äîõîäèì äî êîíöà ñïèñêà:
+	Element<T>* Temp = Head;
+	while (Temp->pNext)
+		Temp = Temp->pNext;
+	//3) Äîáàâëÿåì ýëåìåíò â êîíåö ñïèñêà:
+	Temp->pNext = new Element<T>(Data);
+	size++;
+}
+template<typename T>void ForwardList<T>::insert(int Index, T Data)
+{
+	if (Index == 0)return push_front(Data);
+	if (Index > size)return;
+
+	//1) Ñîçäàåì íîâûé ýëåìåíò:
+	//Element* New = new Element(Data);
+
+	//2) Äîõîäèì äî íóæíîãî ýëåìåíòà
+	Element<T>* Temp = Head;
+	for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+
+	//3) Âñòàâëÿåì íîâûé ýëåìåíò â ñïèñîê:
+	//New->pNext = Temp->pNext;
+	//Temp->pNext = New;
+	Temp->pNext = new Element<T>(Data, Temp->pNext);
+
+	size++;
+}
+
+//					Erasing Elements
+template<typename T>void ForwardList<T>::pop_front()
+{
+	Element<T>* Erased = Head;	//1) çàïîìèíàåì àäðåñ óäàëÿåìîãî ýëåìåíòà
+	Head = Head->pNext;		//2) èñêëþ÷àåì ýëåìåíò èç ñòïèñêà
+	delete Erased;			//3) óäàëÿåì ýëåìåíò èç ïàìÿòè
+
+	size--;
+}
+
+template<typename T>void ForwardList<T>::pop_back()
+{
+	Element<T>* Temp = Head;
+	while (Temp->pNext->pNext)
+	{
+		Temp = Temp->pNext;
+	}
+	delete Temp->pNext;
+	Temp->pNext = nullptr;
+	size--;
+}
+
+//					Methods:
+template<typename T>void ForwardList<T>::print()const
+{
+	/*Element* Temp = Head;	//Temp - ýòî èòåðàòîð
+	//Èòåðàòîð - ýòî óêàçàòåëü, ïðè ïîìîùè êîòîðîãî ìîæíî ïîëó÷èòü äîñòóï
+	//ê ýëåìåíòàì ñòðóêòóðû äàííûõ
+	while (Temp)
+	{
+		cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		Temp = Temp->pNext;	//Ïåðåõîä íà ñëåäóþùèé ýëåìåíò
+	}*/
+	for (Element<T>* Temp = Head; Temp; Temp = Temp->pNext)
+		cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+
+	cout << "Êîëè÷åñòâî ýëåìåíòîâ ñïèñêà:" << size << endl;
+	cout << "Îáùåå êîëè÷åñòâî ýëåìåíòîâ: " << Element::count << endl;
+}
+
+/////////////		CLASS DEFINITION END - ÊÎÍÅÖ ÎÏÐÅÄÅËÅÍÈß ÊËÀÑÑÀ			///////////////
+///////////////////////////////////////////////////////////////////////////////////////////
---
+++ ForwardList/ForwardList.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+#pragma once
+//ForwardList
+#include<iostream>
+#include<string>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////			CLASS DECLARATION - 				///////////////
+
+template<typename T>class ForwardList;
+template<typename T>class Iterator;
+
+template<typename T>class Element
+{
+	T Data;		
+	Element<T>* pNext;	
+	static int count;
+public:
+	Element(T Data, Element<T>* pNext = nullptr);
+	~Element();
+	friend class ForwardList<T>;
+	friend class Iterator<T>;
+};
+
+
+template<typename T>class Iterator
+{
+	Element<T>* Temp;
+public:
+	Iterator(Element<T>* Temp = nullptr);
+	~Iterator();
+
+	Iterator& operator++();
+
+	bool operator==(const Iterator<T>& other)const;
+	bool operator!=(const Iterator<T>& other)const;
+	T& operator*();
+};
+
+template<typename T>class ForwardList	
+{
+	Element<T>* Head;	
+	int size;
+public:
+	Iterator<T> begin();
+	Iterator<T> end();
+	ForwardList();
+	ForwardList(const std::initializer_list<T>& il);
+	~ForwardList();
+
+	//					Adding elements:
+	void push_front(T Data);
+	void push_back(T Data);
+	void insert(int Index, T Data);
+
+	//					Erasing Elements
+	void pop_front();
+	void pop_back();
+
+	//					Methods:
+	void print()const;
+};
+
+/////////////		CLASS DECLARATION END - 		///////////////
+///////////////////////////////////////////////////////////////////////////////////////////
